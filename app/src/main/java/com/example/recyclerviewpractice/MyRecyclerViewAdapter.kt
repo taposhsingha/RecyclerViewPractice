65fb76f9968865ea47cf0e8525f613b6ce73943b
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-09 16:51:55 +0600

    added onClickListener to recyclerview items

--- app/src/main/java/com/example/recyclerviewpractice/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewpractice/MyRecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class MyRecyclerViewAdapter(val fruitsList:List<Fruit>) : RecyclerView.Adapter<MyViewHolder>(){
@@ -28,6 +29,13 @@ class MyViewHolder(val view : View):RecyclerView.ViewHolder(view){
     fun bind (fruit:Fruit){
         val myTextView = view.findViewById<TextView>(R.id.tvName)
         myTextView.text = fruit.name
+        view.setOnClickListener {
+            Toast.makeText(
+                view.context,
+                "Selected Fruit is : ${fruit.name}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 }
